f0c297c7e9e515f3d5d5c83f0b5ec30c9509be50
AndrewPyatunin <andreiandreichgorkin@gmail.com>
2026-06-29 11:20:27 +0300

    Add necessary screens(fix)

---
+++ androidApp/src/main/kotlin/com/andreich/news/ext/NewsItem.kt
@@ -0,0 +1,53 @@
+package com.andreich.news.ext
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.rememberAsyncImagePainter
+import com.andreich.news.domain.model.News
+
+@Composable
+fun NewsItem(news: News, onClickNewsListener: (News) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth().padding(4.dp)
+            .clickable(onClick = {
+                onClickNewsListener(news)
+            }),
+        border = BorderStroke(1.dp, color = Color.Black),
+        shape = CardDefaults.elevatedShape,
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(news.imageUrl),
+                modifier = Modifier.weight(1f),
+                contentDescription = null
+            )
+            Column(modifier = Modifier.padding(horizontal = 4.dp).weight(2f)) {
+                Text(text = news.title, fontSize = 18.sp, fontFamily = FontFamily.Serif)
+                Spacer(Modifier.size(6.dp))
+                Text(text = news.description, fontSize = 14.sp)
+            }
+        }
+    }
+
+}
